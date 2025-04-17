a262f713a4f494f8bd153334aa419aff5c681e04
Sudhanshu Tripathi <sudhaanshutripathi@gmail.com>
2025-04-17 22:19:17 +0530

    QUIZ 1 COMPLETED

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Welcome to My Website!');
 });
+alert('Welcome to My Website!');
+// This is a simple JavaScript file that runs when the DOM is fully loaded.
